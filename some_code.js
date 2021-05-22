37cbc4b59d9439734520731eee76c5ab30d7831a
Zard <td.zard@gmail.com>
2021-05-23 06:17:29 +0700

    Update some_code.js

--- some_code.js
+++ some_code.js
@@ -2,9 +2,7 @@ function setNativeValue(element, value) {
   let lastValue = element.value;
   element.value = value;
   let event = new Event('input', { target: element, bubbles: true });
-  // React 15
   event.simulated = true;
-  // React 16
   let tracker = element._valueTracker;
   if (tracker) {
     tracker.setValue(lastValue);
