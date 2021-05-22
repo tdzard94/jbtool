1a6f950d4ff492482262eda460eee5051a3fe3e0
Zard <td.zard@gmail.com>
2021-05-23 06:16:59 +0700

    Create some_code.js

---
+++ some_code.js
@@ -0,0 +1,31 @@
+function setNativeValue(element, value) {
+  let lastValue = element.value;
+  element.value = value;
+  let event = new Event('input', { target: element, bubbles: true });
+  // React 15
+  event.simulated = true;
+  // React 16
+  let tracker = element._valueTracker;
+  if (tracker) {
+    tracker.setValue(lastValue);
+  }
+  element.dispatchEvent(event);
+}
+let input = document.getElementById('{element}');
+setNativeValue(input, '{value}');
+
+
+const mouseClickEvents = ['mousedown', 'click', 'mouseup'];
+function simulateMouseClick(element){
+  mouseClickEvents.forEach(mouseEventType =>
+    element.dispatchEvent(
+      new MouseEvent(mouseEventType, {
+         view: window,
+         bubbles: true,
+         cancelable: true,
+         buttons: 1
+  })));
+}
+
+let element =document.document.getElementById('username')
+simulateMouseClick(element);
